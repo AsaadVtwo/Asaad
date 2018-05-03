559b3e9da4d2ddafa67610403b3f25d0cbbf8329
asaadvtwo <asaad.v2@gmai.com>
2018-05-03 10:34:27 +0300

    Add Age Extenstion

--- Asaad/Vega.API/Helpers/AutoMapperProfiles.cs
+++ Asaad/Vega.API/Helpers/AutoMapperProfiles.cs
@@ -9,25 +9,25 @@ namespace Vega.API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForDetailedDto>();
-                // .ForMember(dest => dest.PhotoUrl, opt => 
-                // {
-                //     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
-                // })
-                // .ForMember(dest => dest.Age, opt =>
-                // {
-                //     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
-                // });
-            CreateMap<User, UserForListDto>();
-            //     .ForMember(dest => dest.PhotoUrl, opt => 
-            //     {
-            //         opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
-            //     })
-            //     .ForMember(dest => dest.Age, opt =>
-            //     {
-            //         opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
-            //     });
-            // CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt =>
+                {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt =>
+                {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
 }
--- Asaad/Vega.API/Helpers/Extensions.cs
+++ Asaad/Vega.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace Vega.API.Helpers
@@ -9,6 +10,11 @@ namespace Vega.API.Helpers
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+        public static int CalculateAge(this DateTime ThisDateTime)
+        {
+            var age= DateTime.Today.Year-ThisDateTime.Year;
+            return age;
         }
     }
 }
